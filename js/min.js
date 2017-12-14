b8fc19d6f641487ac31a382d267a9423ef59a709
AVSStorage <33234008+AVSStorage@users.noreply.github.com>
2017-12-14 13:41:56 +0300

    Fin3

---
+++ js/min.js
@@ -0,0 +1,1 @@
+var link=document.querySelector(".search-hotels__button"),input=document.querySelector(".input");link.addEventListener("click",function(t){t.preventDefault(),input.classList.toggle("input-show")});
